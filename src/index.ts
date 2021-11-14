f81c434911af22fd49a8c4478dc98d841ef1a480
l.murillo.baltodano <l.murillo.baltodano@accenture.com>
2021-11-13 22:14:01 -0600

    enhance server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
 import { ApolloServer } from 'apollo-server';
 import SchemaBuilder from '@giraphql/core';
+import fakeDB from './fakeDB.json';
+
 const builder = new SchemaBuilder({});
+
 builder.queryType({
-  fields: (t) => ({
-    hello: t.string({
+  fields: (types) => ({
+    person: types.string({
       args: {
-        name: t.arg.string(),
+        name: types.arg.string(),
       },
-      resolve: (_, { name }) => `hello, ${name || 'World'}`,
-    }),
+      resolve: (_,
+        { name }
+      ) => fakeDB.people.find(({name: lookupName}) => {
+
+        console.log('lookupName', lookupName)
+        console.log('name', name)
+
+        return lookupName.includes(name as string)
+      })?.name || 'Default name'
+    })
   }),
 });
 new ApolloServer({
